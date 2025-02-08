29a5a54be247dda0e36cc7bfffa807f20ddda796
vaujan <vaujan@github.com>
2025-02-08 22:58:37 +0700

    checkpoint: chapter 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
+
+export { inter, lusitana };
